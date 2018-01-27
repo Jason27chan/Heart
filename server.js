86a508a0efd550da265ab140c30440f26d00293f
Jason Chan <jason27chan@gmail.com>
2018-01-27 17:36:23 -0500

    delete a bunch of code to add my own

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
 var express = require("express");
 var bodyParser = require("body-parser")
-
 var app = express();
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-var messages = [
-	{name: "Tim", message: "hi"},
-	{name: "Jane", message: "hello"}
-]
-
-app.get('/messages', (req, res) => {
-	res.send(messages)
-})
-
-app.post('/messages', (req, res) => {
-	messages.push(req.body)
-	res.sendStatus(200)
+app.get("./chatroom", (req, res) => {
+	res.get("./chatroom.html")
 })
 
 var server = app.listen(3000, () => {
